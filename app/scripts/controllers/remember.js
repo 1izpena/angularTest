63db274def2bac06bbd817961102b47367c9d0cf
Asier <garcia.a.asier@gmail.com>
2015-12-14 16:19:00 +0100

    Formulario recordar se oculta y muestra mensaje

--- app/scripts/controllers/remember.js
+++ app/scripts/controllers/remember.js
@@ -5,21 +5,30 @@ angular.module('myAppAngularMinApp')
     .controller('RememberCtrl', function ($scope, ResetService, $location, $localStorage) { //nombre del controlador y ambito del scope
 
 //Comprobar q el email existe
-
+ $scope.FormVisibility = true;
    $scope.check = function(){
+  
       $scope.message = '';
       $scope.error = 0;
       if($scope.FormData.mail)
       {
+        //esconder Form
+      $scope.FormVisibility = false;
+      $scope.ResponseVisibility = true;
+
         ResetService.check($scope.FormData).then(function(res)
         {
           $scope.storage = $localStorage.$default({
             ResetToken: res.data
           });
+          
+          
         },function(res)
         {
+          alert(res.data.message);
           $scope.error = 1;
           $scope.message = res.data.message;
+         
         });
       }
     };
