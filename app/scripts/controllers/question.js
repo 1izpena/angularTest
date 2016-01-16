fb1e2656015432ed8325d4081d6ac66ad0796e0e
JonTxira <BICHEGJI@intranet.ibermatica>
2016-01-16 17:41:42 +0100

    añadidas funciones para votar y comentar pregunta

--- app/scripts/controllers/question.js
+++ app/scripts/controllers/question.js
@@ -10,15 +10,59 @@ angular.module('myAppAngularMinApp')
   $scope.error = 0;
   $scope.success = 0;
   $scope.path = $location.path();
+  $scope.showModal = false;
+
   $scope.getQuestion = function(id)
   {
     QuestionService.getQuestion(id).then(function (res){
       $scope.question = res.data;
-      console.log(res.data);
     },function(err){
       $scope.error = err.message;
     });
   }
+
+  $scope.showCommentForm = function ()
+  {
+     $scope.showModal = !$scope.showModal;
+  }
+
+  $scope.comment = function(comment)
+  {
+    $scope.message = '';
+    $scope.error = 0;
+    $scope.success = 0;
+    $scope.showModal = false;
+    QuestionService.commentQuestion($routeParams.questionid,comment).then(function(res){
+      $scope.success =1;
+      $scope.message = res.data;
+    },function(err){
+      $scope.error = 1;
+      $scope.message = err.data.message;
+    });
+  }
+
+  $scope.upvote = function()
+  {
+    QuestionService.upVote($routeParams.questionid).then(function(res){
+      $scope.success =1;
+      $scope.message = res.data;
+    },function(err){
+      $scope.error = 1;
+      $scope.message = err.data.message;
+    });
+  }
+
+  $scope.downvote = function()
+  {
+    QuestionService.downVote($routeParams.questionid).then(function(res){
+      $scope.success =1;
+      $scope.message = res.data;
+    },function(err){
+      $scope.error = 1;
+      $scope.message = err.data.message;
+    });
+  }
+
   $scope.init = function()
   {
     $scope.getQuestion($routeParams.questionid);
--- app/scripts/services/questionservice.js
+++ app/scripts/services/questionservice.js
@@ -4,15 +4,37 @@ angular.module('myAppAngularMinApp')
   .service('QuestionService', ['$http', '$localStorage', '$location', '$q', 'API_BASE',
   	function($http, $localStorage, $location, $q ,API_BASE){
   		return{
-  			getQuestion: getQuestion
+        createQuestion: createQuestion,
+  			getQuestion: getQuestion,
+        commentQuestion: commentQuestion,
+        upVote: upVote,
+        downVote: downVote     
   		};
-  	function getQuestion(id)
+    function createQuestion(data)
+    {
+      var defered = $q.defer();
+      var promise = defered.promise;
+      $http({
+          method: 'post',
+          headers: {'x-access-token': $localStorage.token},
+          url: API_BASE + '/api/v1/forum/question/',
+        }).then(
+          function(response) {
+            defered.resolve(response);
+          },
+          function(error){
+            defered.reject(error);
+          }
+        );
+      return promise;     
+    };
+  	function getQuestion(questionId)
   	{
       var defered = $q.defer();
       var promise = defered.promise;
       $http({
           method: 'get',
-          url: API_BASE + '/api/v1/forum/question/'+ id,
+          url: API_BASE + '/api/v1/forum/question/'+ questionId,
         }).then(
           function(response) {
             defered.resolve(response);
@@ -22,5 +44,73 @@ angular.module('myAppAngularMinApp')
           }
         );
       return promise; 		
-	};
+	  };
+    function commentQuestion(questionId, data)
+    {
+      var defered = $q.defer();
+      var promise = defered.promise;
+      var nowDate = new Date().getTime();
+      $http({
+          method: 'put',
+          headers: {'x-access-token': $localStorage.token},
+          url: API_BASE + '/api/v1/forum/question/'+ questionId+ "/comment",
+          data: {
+            'comment': data,
+            'questionid': questionId,
+            'created': nowDate,
+          }
+        }).then(
+          function(response) {
+            defered.resolve(response);
+          },
+          function(error){
+            defered.reject(error);
+          }
+        );
+      return promise;     
+    };
+    function upVote(questionId)
+    {
+      var defered = $q.defer();
+      var promise = defered.promise;
+      $http({
+          method: 'put',
+          headers: {'x-access-token': $localStorage.token},
+          url: API_BASE + '/api/v1/forum/question/'+ questionId+ "/upvote",
+          data:{
+            "questionid": questionId,
+            "vote": 1
+          }
+        }).then(
+          function(response) {
+            defered.resolve(response);
+          },
+          function(error){
+            defered.reject(error);
+          }
+        );
+      return promise;     
+    };
+    function downVote(questionId)
+    {
+      var defered = $q.defer();
+      var promise = defered.promise;
+      $http({
+          method: 'put',
+          headers: {'x-access-token': $localStorage.token},
+          url: API_BASE + '/api/v1/forum/question/'+ questionId+ "/downvote",
+          data:{
+            "questionid": questionId,
+            "vote": -1
+          }
+        }).then(
+          function(response) {
+            defered.resolve(response);
+          },
+          function(error){
+            defered.reject(error);
+          }
+        );
+      return promise;     
+    };
 }]);
