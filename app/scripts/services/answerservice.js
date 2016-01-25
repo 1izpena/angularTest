2f8a76244e60be27de5c403ab0226507f05ce2f5
JonTxira <BICHEGJI@intranet.ibermatica>
2016-01-26 00:13:33 +0100

    Servicio de respuestas del foro. Añadidas funciones de creación, comentario, votar positivo y votar negativo

---
+++ app/scripts/services/answerservice.js
@@ -0,0 +1,80 @@
+'use strict';
+
+angular.module('myAppAngularMinApp')
+  .service('AnswerService', ['$http', '$localStorage', '$location', '$q', 'API_BASE',
+  	function($http, $localStorage, $location, $q ,API_BASE){
+  		return{
+  			newAnswer: newAnswer,
+        upVote:    upVote,
+        downVote:  downVote
+  		};
+  function newAnswer(questionId,data)
+  {
+    var defered = $q.defer();
+    var promise = defered.promise;
+    var nowDate = new Date().getTime();
+    $http({
+      method: 'post',
+      headers: {'x-access-token': $localStorage.token},
+      url: API_BASE + '/api/v1/forum/question/'+ questionId+ "/answer",
+      data:{
+        "body"      : data.body,
+        "created"   : nowDate,
+        "votes"     : 0
+      }
+    }).then(
+        function(response){
+          defered.resolve(response);
+        },
+        function(error){
+          defered.reject(error);
+        }
+    );
+    return promise;     
+  };
+
+  function upVote(questionId,answerId)
+  {
+    var defered = $q.defer();
+    var promise = defered.promise;
+    $http({
+      method: 'put',
+      headers: {'x-access-token': $localStorage.token},
+      url: API_BASE + '/api/v1/forum/question/'+ questionId+ "/answer/"+answerId+'/upvote',
+      data:{
+        "vote"      : 1,
+      }
+    }).then(
+        function(response){
+          defered.resolve(response);
+        },
+        function(error){
+          defered.reject(error);
+        }
+    );
+    return promise; 
+  };
+
+  function downVote(questionId,answerId)
+  {
+    var defered = $q.defer();
+    var promise = defered.promise;
+    $http({
+      method: 'put',
+      headers: {'x-access-token': $localStorage.token},
+      url: API_BASE + '/api/v1/forum/question/'+ questionId+ "/answer/"+answerId+'/downvote',
+      data:{
+        "vote"      : -1,
+      }
+    }).then(
+        function(response){
+          defered.resolve(response);
+        },
+        function(error){
+          defered.reject(error);
+        }
+    );
+    return promise;
+  }
+
+}]);
