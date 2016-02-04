7759647a772e1d861fc1f4e4f3f20c9686ed8e4d
JonTxira <BICHEGJI@intranet.ibermatica>
2016-02-04 08:29:17 +0100

    Añadidas controlador y vistas para las ventanas modales de añadir comentarios

---
+++ app/scripts/controllers/commentModal.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('myAppAngularMinApp')
+  .controller('commentModalCtrl', ['$scope', '$uibModalInstance',  '$localStorage', 'QuestionService','questionid','answerid','AnswerService',
+    function ($scope, $uibModalInstance, $localStorage, QuestionService, questionid, answerid,AnswerService) {
+    	$scope.answerid=answerid;
+    	$scope.questionid=questionid;
+    	$scope.QuestionComment = function (comment) {
+	      $scope.message = '';
+	      $scope.error = 0;
+	      $scope.success = 0;
+	      QuestionService.commentQuestion(questionid,comment).then(function(res){
+	        $scope.success =1;
+	        $uibModalInstance.close(res.data);
+	      },function(err){
+	        $scope.error = 1;
+	        $scope.message = err.data.message;
+	      });
+	    };
+
+	    $scope.AnswerComment = function (comment){
+		     AnswerService.commentAnswer(questionid,answerid,comment).then(function(res){
+		      $scope.success =1;
+		     $uibModalInstance.close(res.data);
+		    },function(err){
+		      $scope.error = 1;
+		      $scope.message = err.data.message;
+		    });
+	    };
+}]);
