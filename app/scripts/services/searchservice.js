9fe893c1c99b88d8dde5ef37720caf35d643bfae
izaskun_peke_@hotmail.com <izaskun_peke_@hotmail.com>
2016-02-16 15:38:03 +0100

    Modificado metodo de busqueda para el chat para que busque por campos y solo los mensajes del canal en el que esta

--- app/scripts/services/searchservice.js
+++ app/scripts/services/searchservice.js
@@ -4,8 +4,10 @@ angular.module('myAppAngularMinApp')
   .service('searchservice', ['$http', '$localStorage', '$location', '$q', 'API_BASE',
     function($http, $localStorage, $location, $q, API_BASE){
       return{
-        forumsearch: forumsearch
+        forumsearch: forumsearch, 
+        chatsearch: chatsearch
       };
+
     function forumsearch(request)
     {
       var defered = $q.defer();
@@ -38,4 +40,55 @@ angular.module('myAppAngularMinApp')
         );
       return promise;
   };
+
+
+  function chatsearch(text, channelid)
+  {
+      var defered = $q.defer();
+      var promise = defered.promise;
+
+      $http({
+          method: 'POST',
+          url: API_BASE + '/api/v1/chatsearch',
+          data: {key: text, channel: channelid}
+        }).then(
+          function(response) {
+            console.log("entro en .then");
+             if(response.data.hits.hits.length > 0 ){
+              
+
+              var hits = (Object.keys(response.data.hits.hits).length)-1;        
+              var arrayText = [];
+
+          
+              for (var cont = 0; cont <= hits; cont++){
+                
+                var obj = {};
+                obj.id = response.data.hits.hits[ cont ]._id;
+                obj.source = response.data.hits.hits[ cont ]._source;
+                arrayText.push(obj);
+              }
+              
+
+            } else {
+              //console.log(response.data.hits);
+              console.log("no hay coincidencias");
+              arrayText = ({error:"No hay coincidencias"});
+             
+            }
+            //console.log(data);
+            defered.resolve(arrayText);
+            
+          },
+          function(error){
+            console.log("hay error");
+            defered.reject(error);
+          }
+        );
+      return promise;
+  };
+
+
+
+
 }]);
